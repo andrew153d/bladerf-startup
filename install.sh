086091bdb955779ea1e578c3349076945134ae14
andrew153d <109369025+andrew153d@users.noreply.github.com>
2024-12-30 14:31:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,5 @@
-
-# purge existing packages if present #
-# sudo apt purge bladerf libbladerf-dev cubicsdr -y
-rm hostedxA4-latest.rbf
-rm bladeRF_fw_latest.img
-
-sudo apt-get update
-
-# install bladerf#
-# https://github.com/Nuand/bladeRF/wiki/Getting-Started%3A-Linux #
-#dependecies #
-sudo apt-get install libusb-1.0-0-dev libusb-1.0-0 build-essential cmake libncurses5-dev libtecla1 libtecla-dev pkg-config git wget -y
-sudo apt-get install doxygen help2man pandoc -y
-#rm -rf ./bladeRF
-git clone https://github.com/Nuand/bladeRF.git ./bladeRF --recursive
-cd ./bladeRF
-cd host/
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DINSTALL_UDEV_RULES=ON ../
-make
-sudo make install
-sudo ldconfig
-export PATH=$PATH:/usr/local
+# https://github.com/pothosware/SoapySDR/wiki/BuildGuide
 cd ~/
-
-
-wget https://www.nuand.com/fpga/hostedxA4-latest.rbf
-wget https://www.nuand.com/fx3/bladeRF_fw_latest.img
-
-# SoapySDR #
-sudo apt-get install cmake g++ libpython3-dev python3-numpy swig
 git clone https://github.com/pothosware/SoapySDR.git
 cd SoapySDR
 mkdir build
@@ -37,40 +7,48 @@ cd build
 cmake ..
 make -j`nproc`
 sudo make install -j`nproc`
-sudo ldconfig
+sudo ldconfig #needed on debian systems
 SoapySDRUtil --info
 
-#cubicsdr
-sudo apt install cubicsdr -y
+# https://github.com/pothosware/SoapyBladeRF/wiki
+cd ~/
+git clone https://github.com/pothosware/SoapyBladeRF.git
+cd SoapyBladeRF
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+#https://www.rtl-sdr.com/rtl-sdr-quick-start-guide/
+sudo apt purge ^librtlsdr -y
+sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_*
+
+cd ~/
+sudo apt-get install libusb-1.0-0-dev git cmake pkg-config build-essential -y
+git clone https://github.com/rtlsdrblog/rtl-sdr-blog
+cd rtl-sdr-blog/
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
+# https://github.com/pothosware/SoapyRTLSDR/wiki
+cd ~/
+git clone https://github.com/pothosware/SoapyRTLSDR.git
+cd SoapyRTLSDR
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sudo apt update
+sudo apt -y install cubicsdr
