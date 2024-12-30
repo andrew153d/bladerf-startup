67034cfb8b1fcafdb05e22cb3d4405218df8a8b7
andrew153d <109369025+andrew153d@users.noreply.github.com>
2024-12-30 12:13:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,19 @@ sudo apt-get update
 
 # install bladerf#
 # https://github.com/Nuand/bladeRF/wiki/Getting-Started%3A-Linux #
-rm -rf ./bladeRF
+#dependecies #
+sudo apt-get install libusb-1.0-0-dev libusb-1.0-0 build-essential cmake libncurses5-dev libtecla1 libtecla-dev pkg-config git wget -y
+sudo apt-get install doxygen help2man pandoc -y
+#rm -rf ./bladeRF
 git clone https://github.com/Nuand/bladeRF.git ./bladeRF --recursive
 cd ./bladeRF
 cd host/
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DINSTALL_UDEV_RULES=ON ../
-make && sudo make install && sudo ldconfig
+make
+sudo make install
+sudo ldconfig
 export PATH=$PATH:/usr/local
 
 wget https://www.nuand.com/fpga/hostedxA4-latest.rbf
@@ -30,11 +35,11 @@ cd build
 cmake ..
 make -j`nproc`
 sudo make install -j`nproc`
-sudo ldconfig #needed on debian systems
+sudo ldconfig
 SoapySDRUtil --info
 
 #cubicsdr
-sudo apt install cubicsdr
+sudo apt install cubicsdr -y
 
 
 
