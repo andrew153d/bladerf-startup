8084bf87b3d6d23edca7c2d553cc65fe71476e87
andrew153d <109369025+andrew153d@users.noreply.github.com>
2024-12-30 11:53:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,24 +6,6 @@ rm bladeRF_fw_latest.img
 
 sudo apt-get update
 
-# setup networking #
-echo -e "# Let NetworkManager manage all devices on this system\n\
-network:\n\
-  version: 2\n\
-  renderer: NetworkManager\n\
-  ethernets:\n\
-        eth0:\n\
-            addresses:\n\
-                - 192.168.12.234/24\n\
-            nameservers:\n\
-                addresses: [8.8.8.8, 8.8.4.4]\n\
-            routes:\n\
-                - to: default\n\
-                  via: 192.168.12.1" | sudo tee /etc/netplan/01-network-manager-all.yaml > /dev/null
-
-sudo netplan apply
-sudo systemctl restart network-manager
-
 # install bladerf#
 # https://github.com/Nuand/bladeRF/wiki/Getting-Started%3A-Linux #
 
