36412f4ff23c58471de325f181012623f7ffb6ef
andrew <andrewwisnergrow@gmail.com>
2024-12-31 16:27:41 -0500

    fix logging for part2

--- FullAuto.sh
+++ FullAuto.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 LOG_FILE="$SCRIPT_DIR/startup.log"
-
 exec > >(tee -a "$LOG_FILE") 2>&1
 
 #remove any previous calls from crontab
@@ -13,4 +9,4 @@ crontab -l | grep -v 'SdrPart2' | crontab -
 #add the call
 (crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/SdrPart2.sh") | crontab -
 
-sudo "$SCRIPT_DIR/SdrPart1.sh"
+sudo "$SCRIPT_DIR/SdrPart1.sh" >> "$LOG_FILE" 2>&1
