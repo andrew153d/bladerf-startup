60b38fbae1b0b4c1157bfaf308bbee911b94ca60
andrew <andrewwisnergrow@gmail.com>
2024-12-31 15:51:23 -0500

    fix sudo

--- FullAuto.sh
+++ FullAuto.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 (crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/SdrPart2.sh") | crontab -
 
-"$SCRIPT_DIR/SdrPart1.sh"
+sudo "$SCRIPT_DIR/SdrPart1.sh"
