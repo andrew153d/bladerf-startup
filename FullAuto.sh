5129b6b5cb0f9a8c9a05f9e553c58e550dd562d6
andrew <andrewwisnergrow@gmail.com>
2024-12-31 16:30:30 -0500

    logging changes

--- FullAuto.sh
+++ FullAuto.sh
@@ -7,6 +7,6 @@ exec > >(tee -a "$LOG_FILE") 2>&1
 #remove any previous calls from crontab
 crontab -l | grep -v 'SdrPart2' | crontab -
 #add the call
-(crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/SdrPart2.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/SdrPart2.sh") | crontab - >> "$LOG_FILE" 2>&1
 
 sudo "$SCRIPT_DIR/SdrPart1.sh" >> "$LOG_FILE" 2>&1
