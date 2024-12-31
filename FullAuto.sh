706cae58f4c32b86ec896ff4481f7919f2901db5
andrew <andrewwisnergrow@gmail.com>
2024-12-31 16:00:16 -0500

    check for existing crontab entries

--- FullAuto.sh
+++ FullAuto.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+#remove any previous calls from crontab
+crontab -l | grep -v 'SdrPart2' | crontab -
+#add the call
 (crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/SdrPart2.sh") | crontab -
 
 sudo "$SCRIPT_DIR/SdrPart1.sh"
