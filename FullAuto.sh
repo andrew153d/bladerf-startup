ac7228029387c6f267d6114931cfec02330a3cf8
andrew <andrewwisnergrow@gmail.com>
2024-12-31 16:20:12 -0500

    fix logging

--- FullAuto.sh
+++ FullAuto.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-LOG_FILE="/home/andrew/Documents/VersionControl/bladerf-startup/startup.log"
-exec > >(tee -a "$LOG_FILE") 2>&1
+
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+LOG_FILE="$SCRIPT_DIR/startup.log"
+
+exec > >(tee -a "$LOG_FILE") 2>&1
 
 #remove any previous calls from crontab
 crontab -l | grep -v 'SdrPart2' | crontab -
