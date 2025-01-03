aebcf64d4a7c68c9b6c2c2ab8ff6ce72c56cad09
andrew153d <109369025+andrew153d@users.noreply.github.com>
2025-01-03 10:56:48 -0500

    Update SdrPart1.sh

--- SdrPart1.sh
+++ SdrPart1.sh
@@ -55,7 +55,8 @@ mkdir -p /opt/bladeRF/lib
 echo "* Clone bladeRF repository"
 git clone https://github.com/Nuand/bladeRF --recursive
 echo "* Apt install needed dependencies.."
- apt install python3-six python-six python3-lxml libusb-1.0-0-dev libusb-1.0-0 build-essential cmake libncurses5-dev libtecla1 libtecla-dev pkg-config git wget -y
+apt install python3-six python3-lxml libusb-1.0-0-dev libusb-1.0-0 build-essential cmake libncurses5-dev libtecla1 libtecla-dev pkg-config git wget -y
+apt install cmake make gcc -y
 echo "* Starting Build for BladeRF host software.."
 echo " Change Directory to bladeRF/host..."
 cd bladeRF/host
